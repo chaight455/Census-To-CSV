e9b8dab5629de1292c6b92c08dac4e2e0b377b64
chaight <chaight@wisc.edu>
2024-06-11 10:23:44 -0500

    small bug fix

--- census_app.py
+++ census_app.py
@@ -6,27 +6,32 @@ from io import BytesIO,StringIO
 
 app = Flask(__name__)
 
-"""
-Get Geographical data for given state.
 
-Arguments:
-state_name     #State name eg. "Wisconsin"
-year           #Year of data
 
-Returns Geopandas DataFrame.
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-def get_geo_data(state_name, year):
-    state_code = get_state_code(state_name)
-    # Load in tract data
-    tract_url = f"https://www2.census.gov/geo/tiger/TIGER{year}/TRACT/tl_{year}_{state_code}_tract.zip"
-    response = requests.request("GET", tract_url)
-    data = response.content
-    # Convert data to GeoDataFrame
-    geo_tract_data = read_file(BytesIO(data))
-    return geo_tract_data
-"""
+#AMERICAN COMMUNITY SURVEY PULLERS
+@app.route("/data_profiles", methods=['GET', 'POST'])
+def data_profiles():
+    if request.method == 'POST':
+        return acs_post_helper("data_profiles.html",request)
+    return render_template("data_profiles.html")
 
-"""Helper Method for class"""
+@app.route("/subject_tables", methods=['GET', 'POST'])
+def subject_tables():
+    if request.method == 'POST':
+        return acs_post_helper("subject_tables.html",request)
+    return render_template("subject_tables.html", error = "")
+
+@app.route("/detailed_tables", methods=['GET', 'POST'])
+def detailed_tables():
+    if request.method == 'POST':
+        return acs_post_helper("detailed_tables.html",request)
+    return render_template("detailed_tables.html", error = "")
+
+"""Helper Method for ACS"""
 def get_state_and_county_code(state_name,county_name, census_api_key):
     # URL for getting county and state codes
     states_url = f"https://api.census.gov/data/2022/acs/acs5?get=NAME,B01001_001E&for=county:*&in=state:*&key={census_api_key}"
@@ -70,46 +75,71 @@ def get_state_code(state_name, census_api_key):
         return -1
     return state_code
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def acs_post_helper(html,request):
+    census_api_key = request.form['key']
+    if census_api_key == "":
+        return render_template(html, error = "Error: All boxes must be filled in.")
+    if len(census_api_key) != 40:
+        return render_template(html, error = "Error: Invalid Census API key")
+    variables = request.form['variables']
+    state_name = request.form['state']
+    county_name = request.form['county']
+    year = request.form['year']
+    if (variables == "") or (state_name == "") or (county_name == "") or (year == ""):
+        return render_template(html, error = "Error: All boxes must be filled in.")
+    if int(year)>2022:
+        return render_template(html, error = "Error: Invalid year")
+    state_code = 0
+    county_code = 0
+    if (state_name != "*") and (county_name != "*"):
+        state_code, county_code = get_state_and_county_code(state_name,county_name,census_api_key)
+    elif (county_name == "*") and (state_name != "*"):
+        state_code = get_state_code(state_name,census_api_key)
+        county_code = "*"
+    elif (state_name == "*") and (county_name == "*"):
+      state_code = "*"
+      county_code = "*"
+    if state_code == -1:
+        return render_template(html, error = f"Error: State or County is incorrect. Make sure you include \"County\" after the county name.")
+    variables = variables.replace(" ", "").replace("\"","")
+    # URL for ACS data
+    url = ""
+    if html=="detailed_tables.html":
+      url = f"https://api.census.gov/data/{year}/acs/acs5?get=NAME,{variables}&for=tract:*&in=state:{state_code}&in=county:{county_code}&key={census_api_key}"
+    if html=="subject_tables.html":
+      url = f"https://api.census.gov/data/{year}/acs/acs5/subject?get=NAME,{variables}&for=tract:*&in=state:{state_code}&in=county:{county_code}&key={census_api_key}"
+    if html=="data_profiles.html":
+      url = f"https://api.census.gov/data/{year}/acs/acs5/profile?get=NAME,{variables}&for=tract:*&in=state:{state_code}&in=county:{county_code}&key={census_api_key}"
+    # Request ACS data
+    response = requests.request("GET", url)
+    if response.status_code != 200:
+        return render_template(html, error = f"Error: Variables entered do not exist")
+    csv_data = DataFrame(response.json()[1:], columns=response.json()[0]).to_csv()
+    # Create StringIO object to hold CSV data in memory
+    csv_stream = StringIO()
+    csv_stream.write(csv_data)
+    csv_stream.seek(0)
 
-@app.route("/data_profiles", methods=['GET', 'POST'])
-def data_profiles():
+    # Return CSV file as a response
+    return Response(
+        csv_stream,
+        mimetype="text/csv",
+        headers={"Content-disposition": "attachment; filename=data.csv"}
+    )
+
+#GEO DATA PULLER
+@app.route("/geo_tract", methods=['GET', 'POST'])
+def geo_tract():
     if request.method == 'POST':
-        census_api_key = request.form['key']
-        if census_api_key == "":
-            return render_template("data_profiles.html", error = "Error: All boxes must be filled in.")
-        if len(census_api_key) != 40:
-            return render_template("data_profiles.html", error = "Error: Invalid Census API key")
-        variables = request.form['variables']
         state_name = request.form['state']
-        county_name = request.form['county']
         year = request.form['year']
-        if (variables == "") or (state_name == "") or (county_name == "") or (year == ""):
-            return render_template("data_profiles.html", error = "Error: All boxes must be filled in.")
-        if int(year)>2022:
-            return render_template("data_profiles.html", error = "Error: Invalid year")
-        state_code = 0
-        county_code = 0
-        if (state_name != "*") and (county_name != "*"):
-            state_code, county_code = get_state_and_county_code(state_name,county_name,census_api_key)
-        elif (county_name == "*") and (state_name != "*"):
-            state_code = get_state_code(state_name,census_api_key)
-            county_code = "*"
-        elif (state_name == "*") and (county_name == "*"):
-          state_code = "*"
-          county_code = "*"
-        if state_code == -1:
-            return render_template("data_profiles.html", error = f"Error: State or County is incorrect. Make sure you include \"County\" after the county name.")
-        variables = variables.replace(" ", "")
-        # URL for ACS data
-        survey_url = f"https://api.census.gov/data/{year}/acs/acs5/profile?get=NAME,{variables}&for=tract:*&in=state:{state_code}&in=county:{county_code}&key={census_api_key}"
-        print(survey_url)
-        # Request ACS data
-        response = requests.request("GET", survey_url)
+        key = request.form['key']
+        state_code = get_state_code(state_name, key)
+        # Load in tract data
+        tract_url = f"https://www2.census.gov/geo/tiger/TIGER{year}/TRACT/tl_{year}_{state_code}_tract.zip"
+        response = requests.request("GET", tract_url)
         if response.status_code != 200:
-            return render_template("data_profiles.html", error = f"Error: To see your error visit {survey_url}")
+            return render_template("geo_polygon", error = f"Error: Variables entered do not exist")
         csv_data = DataFrame(response.json()[1:], columns=response.json()[0]).to_csv()
         # Create StringIO object to hold CSV data in memory
         csv_stream = StringIO()
@@ -122,107 +152,7 @@ def data_profiles():
             mimetype="text/csv",
             headers={"Content-disposition": "attachment; filename=data.csv"}
         )
-    return render_template("data_profiles.html")
-
-@app.route("/subject_tables", methods=['GET', 'POST'])
-def subject_tables():
-    if request.method == 'POST':
-        census_api_key = request.form['key']
-        if census_api_key == "":
-            return render_template("subject_tables.html", error = "Error: All boxes must be filled in.")
-        if len(census_api_key) != 40:
-            return render_template("subject_tables.html", error = "Error: Invalid Census API key")
-        variables = request.form['variables']
-        state_name = request.form['state']
-        county_name = request.form['county']
-        year = request.form['year']
-        if (variables == "") or (state_name == "") or (county_name == "") or (year == ""):
-            return render_template("subject_tables.html", error = "Error: All boxes must be filled in.")
-        if int(year)>2022:
-            return render_template("subject_tables.html", error = "Error: Invalid year")
-        state_code = 0
-        county_code = 0
-        if (state_name != "*") and (county_name != "*"):
-            state_code, county_code = get_state_and_county_code(state_name,county_name,census_api_key)
-        elif (county_name == "*") and (state_name != "*"):
-            state_code = get_state_code(state_name,census_api_key)
-            county_code = "*"
-        elif (state_name == "*") and (county_name == "*"):
-          state_code = "*"
-          county_code = "*"
-        if state_code == -1:
-            return render_template("subject_tables.html", error = f"Error: State or County is incorrect. Make sure you include \"County\" after the county name.")
-        variables = variables.replace(" ", "")
-        # URL for ACS data
-        survey_url = f"https://api.census.gov/data/{year}/acs/acs5/subject?get=NAME,{variables}&for=tract:*&in=state:{state_code}&in=county:{county_code}&key={census_api_key}"
-        print(survey_url)
-        # Request ACS data
-        response = requests.request("GET", survey_url)
-        if response.status_code != 200:
-            return render_template("subject_tables.html", error = f"Error: To see your error visit {survey_url}")
-        csv_data = DataFrame(response.json()[1:], columns=response.json()[0]).to_csv()
-        # Create StringIO object to hold CSV data in memory
-        csv_stream = StringIO()
-        csv_stream.write(csv_data)
-        csv_stream.seek(0)
-
-        # Return CSV file as a response
-        return Response(
-            csv_stream,
-            mimetype="text/csv",
-            headers={"Content-disposition": "attachment; filename=data.csv"}
-        )
-    return render_template("subject_tables.html")
-
-@app.route("/detailed_tables", methods=['GET', 'POST'])
-def detailed_tables():
-    if request.method == 'POST':
-        census_api_key = request.form['key']
-        if census_api_key == "":
-            return render_template("detailed_tables.html", error = "Error: All boxes must be filled in.")
-        if len(census_api_key) != 40:
-            return render_template("detailed_tables.html", error = "Error: Invalid Census API key")
-        variables = request.form['variables']
-        state_name = request.form['state']
-        county_name = request.form['county']
-        year = request.form['year']
-        if (variables == "") or (state_name == "") or (county_name == "") or (year == ""):
-            return render_template("detailed_tables.html", error = "Error: All boxes must be filled in.")
-        if int(year)>2022:
-            return render_template("detailed_tables.html", error = "Error: Invalid year")
-        state_code = 0
-        county_code = 0
-        if (state_name != "*") and (county_name != "*"):
-            state_code, county_code = get_state_and_county_code(state_name,county_name,census_api_key)
-        elif (county_name == "*") and (state_name != "*"):
-            state_code = get_state_code(state_name,census_api_key)
-            county_code = "*"
-        elif (state_name == "*") and (county_name == "*"):
-          state_code = "*"
-          county_code = "*"
-        if state_code == -1:
-            return render_template("detailed_tables.html", error = f"Error: State or County is incorrect. Make sure you include \"County\" after the county name.")
-        variables = variables.replace(" ", "")
-        # URL for ACS data
-        survey_url = f"https://api.census.gov/data/{year}/acs/acs5?get=NAME,{variables}&for=tract:*&in=state:{state_code}&in=county:{county_code}&key={census_api_key}"
-        print(survey_url)
-        # Request ACS data
-        response = requests.request("GET", survey_url)
-        if response.status_code != 200:
-            return render_template("detailed_tables.html", error = f"Error: To see your error visit {survey_url}")
-        csv_data = DataFrame(response.json()[1:], columns=response.json()[0]).to_csv()
-        # Create StringIO object to hold CSV data in memory
-        csv_stream = StringIO()
-        csv_stream.write(csv_data)
-        csv_stream.seek(0)
-
-        # Return CSV file as a response
-        return Response(
-            csv_stream,
-            mimetype="text/csv",
-            headers={"Content-disposition": "attachment; filename=data.csv"}
-        )
-    return render_template("detailed_tables.html")
-
+    return render_template("geo_tract.html", error = "")
+    
 if __name__ == '__main__':
     app.run()
